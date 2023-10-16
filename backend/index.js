6198b14ebdc6176ff12c7d81416ed6dc28afaf10
Margarita Iglesias <margaritaigle99@gmail.com>
2023-10-16 18:10:33 -0300

    adding more classes for creating generations

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,47 @@
-const Dragon = require("./dragon.js");
+const GenerationEngine = require('./engine.js');
+// const Generation = require('./generation.js');
+// const Dragon = require("./dragon.js");
 
-const viky = new Dragon({ birthdate: new Date(), nickname: "viky" });
-const baloo = new Dragon({ nickname: "baloo", birthdate: new Date() });
-const numar = new Dragon();
+
+// * 3. Generation Engine
+
+const engine = new GenerationEngine();
+
+engine.start();
 
 setTimeout(() => {
-    const gooby = new Dragon();
-    console.log(gooby)
-    
-}, [3000])
+    engine.stop();
+}, 20000)
 
-console.log(viky, baloo, numar)
+// * 2. Generation class
+
+// const generation = new Generation;
+
+// console.log('generation', generation);
+
+// const Gooby = generation.newDragon();
+
+// console.log('Gooby', Gooby);
+
+// setTimeout(() => {
+//     const mimar = generation.newDragon();
+//     console.log('mimar', mimar)
+// }, 15000); FAILS
+
+
+// * 1. Dragon class
+
+// const viky = new Dragon({ birthdate: new Date(), nickname: "viky" });
+// const baloo = new Dragon({
+//   nickname: "baloo",
+//   birthdate: new Date(),
+//   traits: [{ traitType: "backgroundColor", traitValue: "blue" }],
+// });
+// const numar = new Dragon();
+
+// setTimeout(() => {
+//   const gooby = new Dragon();
+//   console.log(gooby);
+// }, [3000]);
+
+// console.log(viky, baloo, numar);
