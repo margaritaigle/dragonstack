be7c8f0f99cb95a0a50f356f98aaec7f332df78b
Margarita Iglesias <margaritaigle99@gmail.com>
2023-10-28 23:27:48 -0300

    adding generationId

--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -4,13 +4,19 @@ class GenerationTable {
   // static method can be called as GenerationTable.storeGeneration and cannot be called in a new instance
   // const genTable = new GenerationTable().storeGeneration doesn't work
   static storeGeneration(generation) {
-    pool.query(
-      "INSERT INTO generation(expiration) VALUES($1)",
-      [generation.expiration],
-      (error, response) => {
-        if (error) return console.error(error);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      pool.query(
+        "INSERT INTO generation(expiration) VALUES($1) RETURNING id",
+        [generation.expiration],
+        (error, response) => {
+          if (error) return reject(error);
+
+          const generationId = response.rows[0].id;
+
+          resolve({ generationId });
+        }
+      );
+    });
   }
 }
 
